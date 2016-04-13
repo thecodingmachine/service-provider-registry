0615c4aae7162bc8757cba8b2fed2e7ba3350e38
David Négrier <d.negrier@thecodingmachine.com>
2016-04-13 19:12:18 +0200

    Code cleanup

--- tests/Fixtures/TestServiceProvider.php
+++ tests/Fixtures/TestServiceProvider.php
@@ -2,9 +2,6 @@
 
 namespace TheCodingMachine\ServiceProvider\Fixtures;
 
-use Assembly\ParameterDefinition;
-use Assembly\Reference;
-use Interop\Container\ContainerInterface;
 use Interop\Container\ServiceProvider;
 
 class TestServiceProvider implements ServiceProvider
